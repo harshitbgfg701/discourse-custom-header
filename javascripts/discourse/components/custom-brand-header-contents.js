6b147eacb227bb6818ea8e72b06f635a8f737312
harshitbgfg701 <harshit.dave@bgfg.co.uk>
2024-04-18 14:07:07 +0100

    added bgfg api to fetch html data

--- javascripts/discourse/components/custom-brand-header-contents.js
+++ javascripts/discourse/components/custom-brand-header-contents.js
@@ -6,12 +6,22 @@ export default class CustomBrandHeaderContents extends Component {
 
     get headerHTML() {
 
-        return fetch('https://jsonplaceholder.typicode.com/todos/1')
+        const header = fetch('https://static.bgfg.co.uk/components/videogamer/header/index')
             .then(response => response.json())
             .then(json => {
                 console.log(json);
                 return json;
             });
-    }
 
+        console.log('header data', header);
+
+        console.log('html data', header.html);
+
+        const decodedHTML = Buffer.from(header.html, 'base64')
+        var htmlString = decodedHTML.toString();
+
+        console.log('html data', htmlString);
+
+        return header;
+    }
 }
