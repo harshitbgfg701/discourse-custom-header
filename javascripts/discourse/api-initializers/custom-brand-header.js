970023eb1529d5a1124d609b8a969dc072e80982
harshitbgfg701 <harshit.dave@bgfg.co.uk>
2024-04-18 09:55:40 +0100

    fake api call added

--- javascripts/discourse/api-initializers/custom-brand-header.js
+++ javascripts/discourse/api-initializers/custom-brand-header.js
@@ -1,5 +1,11 @@
 import { apiInitializer } from "discourse/lib/api";
 
-export default apiInitializer("1.8.0", (api) => {
+export default apiInitializer("1.8.0", async (api) => {
   console.log("hello world from api initializer!", api);
+
+  const resp = await fetch('https://jsonplaceholder.typicode.com/todos');
+
+  const data = await resp.json();
+
+  console.log('data coming from jsonplace holder todo api', data);
 });
